6d7217479f85aa80c76e20e92f060417766d953a
nadsphere <omgnnh@gmail.com>
2022-02-26 21:11:04 +0700

    Adding Open Model in Gatling

--- src/test/java/conduitApp/performance/PerfTest.scala
+++ src/test/java/conduitApp/performance/PerfTest.scala
@@ -14,7 +14,14 @@ class PerfTest extends Simulation {
 
   setUp(
     createArticle.inject(
-      atOnceUsers(3)
+        atOnceUsers(1),
+        // Add Open Model
+        nothingFor(4.seconds),
+        constantUsersPerSec(1).during(10.seconds),  // user ketika create article maupun delete
+        constantUsersPerSec(2).during(10.seconds),
+        rampUsersPerSec(2).to(10).during(20.seconds), // bikin load skenario
+        nothingFor(5.seconds),
+        constantUsersPerSec(1).during(5.seconds) // use ".seconds"
     ).protocols(protocol)
   )
 
